0334225415ce91dc256a0d9395058e70492c468d
Luke Ferley <lukeferley6@gmail.com>
2026-07-10 22:08:29 +0200

    feat: implement get request routing.

--- src/get_handler.py
+++ src/get_handler.py
@@ -1,8 +1,13 @@
-import json 
-from db import table 
+from db import table
+from response import ok, error 
+from botocore.exceptions import ClientError
 from typing import Any
 from aws_lambda_typing.context import Context
 from aws_lambda_typing.events import APIGatewayProxyEventV1
 
 def get_handler(event: APIGatewayProxyEventV1, context: Context) -> dict[str, Any]:
-    pass
+    try:
+        content = table.scan()["Items"]
+        return ok(content)
+    except ClientError as e:
+        return error(500, "Could not fetch notes.")
