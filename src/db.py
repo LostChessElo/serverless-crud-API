292f784f72365b790edd4028def1f8857217d294
Luke Ferley <lukeferley6@gmail.com>
2026-07-11 15:49:47 +0200

    fix: fix imports for lambda package

--- src/db.py
+++ src/db.py
@@ -1,8 +1,10 @@
 import boto3
-from mypy_boto3_dynamodb.service_resource import Table
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from mypy_boto3_dynamodb.service_resource import Table
 
 
-def get_table() -> Table:
+def get_table() -> "Table":
     resource = boto3.resource("dynamodb")
     return resource.Table("notes")
 
